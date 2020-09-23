7573c4d8fe5f466c675ab44034f63e8f0ababf6e
RafaS92 <manrvaldez..92@gmail.com>
2020-09-23 14:02:50 -0500

    icons ready

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { Avatar, IconButton } from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sideBar">
-      <h1>Conected</h1>
       <div className="sidebar_header">
-        <div className="sidebar_headerRight"></div>
+        <Avatar src="https://miro.medium.com/fit/c/336/336/1*pybhnKNls5s3RSubfSJ3Hg.jpeg" />
+        <div className="sidebar_headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
