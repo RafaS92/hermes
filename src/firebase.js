50ef395175e84eaa516309791019aa89f653ccff
RafaS92 <manrvaldez..92@gmail.com>
2020-09-23 10:54:46 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkt0GKLH4-4i483ci-EJ9lvHLkIjiaBCw",
+  authDomain: "hermes-65215.firebaseapp.com",
+  databaseURL: "https://hermes-65215.firebaseio.com",
+  projectId: "hermes-65215",
+  storageBucket: "hermes-65215.appspot.com",
+  messagingSenderId: "339173569115",
+  appId: "1:339173569115:web:9147a9088e80bb6415457e",
+};
