e2d88383be946e3464c1acc3ce79d8c18f78b0af
RafaS92 <manrvaldez..92@gmail.com>
2020-09-28 09:51:39 -0500

    pusher working on messages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import Pusher from "pusher-js";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("50c27c33b716da9f78eb", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app_body">
